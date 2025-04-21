dbbfb50a6640ee0c89ad2f7307f899bfcff827c8
Francisco Manuel de Cespedes Díaz <francisco.manuel@civica-soft.com>
2025-04-21 13:08:36 +0200

    Add Criteria Pattern

---
+++ library-management/src/main/java/com/francisco/library_management/domain/filter/Criteria.java
@@ -0,0 +1,22 @@
+package com.francisco.library_management.domain.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Criteria {
+
+	private List<Filter> filters;
+	
+	public Criteria() {
+		filters = new ArrayList<>();
+	}
+	
+	public void addFilter(Filter filter) {
+		filters.add(filter);
+	}
+	
+	public List<Filter> getFilter() {
+		return filters;
+	}
+	
+}

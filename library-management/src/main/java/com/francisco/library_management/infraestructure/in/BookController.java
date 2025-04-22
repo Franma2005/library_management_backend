15e696c538638090eb82f2e043ada3563fff755d
Francisco Manuel de Cespedes Díaz <francisco.manuel@civica-soft.com>
2025-04-22 10:13:05 +0200

    Controller without implement

---
+++ library-management/src/main/java/com/francisco/library_management/infraestructure/in/BookController.java
@@ -0,0 +1,10 @@
+package com.francisco.library_management.infraestructure.in;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+}

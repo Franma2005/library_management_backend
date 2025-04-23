9c559381f6b1be6d928fd51d3be85d430fc68504
Francisco Manuel de Cespedes Díaz <francisco.manuel@civica-soft.com>
2025-04-23 13:02:57 +0200

    Criteria repositories

---
+++ library-management/src/main/java/com/francisco/library_management/application/services/book/BookByCriteriaImpl.java
@@ -0,0 +1,17 @@
+package com.francisco.library_management.application.services.book;
+
+import java.util.List;
+
+import com.francisco.library_management.application.services.servicesInterfaces.book.BookByCriteria;
+import com.francisco.library_management.domain.filter.Criteria;
+import com.francisco.library_management.domain.models.Book;
+
+public class BookByCriteriaImpl implements BookByCriteria {
+
+	@Override
+	public List<Book> getBookByCriteria(Criteria criteria) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

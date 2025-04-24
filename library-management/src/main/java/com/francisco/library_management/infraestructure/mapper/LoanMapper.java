18c56ea43286ef5d90592614f65e8163e74d2bd3
Francisco Manuel de Cespedes Díaz <francisco.manuel@civica-soft.com>
2025-04-24 11:54:23 +0200

    mappers finish

--- library-management/src/main/java/com/francisco/library_management/infraestructure/mapper/LoanMapper.java
+++ library-management/src/main/java/com/francisco/library_management/infraestructure/mapper/LoanMapper.java
@@ -1,5 +1,26 @@
 package com.francisco.library_management.infraestructure.mapper;
 
+import com.francisco.library_management.domain.models.Loan;
+import com.francisco.library_management.infraestructure.dto.LoanDto;
+import com.francisco.library_management.infraestructure.recive.LoanRecive;
+
 public class LoanMapper {
 
+	public static Loan loanRecivetoLoan(LoanRecive loanRecive) {
+		return Loan.builder()
+				.idLoan(loanRecive.getIdLoan())
+				.idBook(loanRecive.getIdBook())
+				.idLibraryUser(loanRecive.getIdLibraryUser())
+				.time(loanRecive.getTime())
+				.build();
+	}
+	
+	public static LoanDto loantoLoanDto(Loan loan) {
+		return LoanDto.builder()
+				.idLoan(loan.getIdLoan())
+				.idBook(loan.getIdBook())
+				.idLibraryUser(loan.getIdLibraryUser())
+				.time(loan.getTime())
+				.build();
+	}
 }

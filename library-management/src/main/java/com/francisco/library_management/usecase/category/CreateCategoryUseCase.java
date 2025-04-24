46ac1799607e8c3bea24488587cdaf283e46da98
Francisco Manuel de Cespedes Díaz <francisco.manuel@civica-soft.com>
2025-04-24 13:49:39 +0200

    creates usecase make

--- library-management/src/main/java/com/francisco/library_management/usecase/category/CreateCategoryUseCase.java
+++ library-management/src/main/java/com/francisco/library_management/usecase/category/CreateCategoryUseCase.java
@@ -3,7 +3,10 @@ package com.francisco.library_management.usecase.category;
 import org.springframework.http.ResponseEntity;
 
 import com.francisco.library_management.application.services.category.CreateCategoryImpl;
+import com.francisco.library_management.domain.models.Category;
 import com.francisco.library_management.infraestructure.dto.CategoryDto;
+import com.francisco.library_management.infraestructure.mapper.CategoryMapper;
+import com.francisco.library_management.infraestructure.recive.CategoryRecive;
 
 public class CreateCategoryUseCase {
 
@@ -13,7 +16,10 @@ public class CreateCategoryUseCase {
 		this.createCategoryImpl = createCategoryImpl;
 	}
 	
-	public ResponseEntity<CategoryDto> createCategory() {
-		
+	public ResponseEntity<CategoryDto> createCategory(CategoryRecive categoryRecive) {
+		Category category = CategoryMapper.categoryRecivetoCategory(categoryRecive);
+		createCategoryImpl.createCategory(category);
+		CategoryDto categoryDto = CategoryMapper.categorytoCategoryDto(category);
+		return ResponseEntity.ok(categoryDto);
 	}
 }
--- library-management/src/main/java/com/francisco/library_management/usecase/libraryUser/CreateLibraryUserUseCase.java
+++ library-management/src/main/java/com/francisco/library_management/usecase/libraryUser/CreateLibraryUserUseCase.java
@@ -1,5 +1,26 @@
 package com.francisco.library_management.usecase.libraryUser;
 
+import org.springframework.http.ResponseEntity;
+
+import com.francisco.library_management.application.services.libraryUser.CreateLibraryUserImpl;
+import com.francisco.library_management.domain.models.LibraryUser;
+import com.francisco.library_management.infraestructure.dto.LibraryUserDto;
+import com.francisco.library_management.infraestructure.mapper.LibraryUserMapper;
+import com.francisco.library_management.infraestructure.recive.LibraryUserRecive;
+
 public class CreateLibraryUserUseCase {
 
+	private CreateLibraryUserImpl createLibraryUserImpl;
+	
+	public CreateLibraryUserUseCase(CreateLibraryUserImpl createLibraryUserImpl) {
+		this.createLibraryUserImpl = createLibraryUserImpl;
+	}
+	
+	public ResponseEntity<LibraryUserDto> createLibraryUser(LibraryUserRecive libraryUserRecive) {
+		LibraryUser libraryUser = LibraryUserMapper.libraryUserRecivetoLibraryUser(libraryUserRecive);
+		createLibraryUserImpl.createLibraryUser(libraryUser);
+		LibraryUserDto libraryUserDto = LibraryUserMapper.libraryUsertoLibraryUserDto(libraryUser);
+		return ResponseEntity.ok(libraryUserDto);
+	}
+	
 }
